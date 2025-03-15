ad2236eb21823b953316d7ba115c0dabb6c21154
dexterlb <dexterlb@qtrp.org>
2025-03-15 11:51:43 +0200

    migrate from poetry to UV

--- flake.nix
+++ flake.nix
@@ -4,51 +4,96 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, uv2nix, pyproject-nix, pyproject-build-systems }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        lib = nixpkgs.lib;
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication defaultPoetryOverrides;
+        python = pkgs.python312;
+
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        };
+        pyprojectOverrides = final: prev: {
+          # some dependencies need specific overrides
+          jsmin = prev.jsmin.overrideAttrs (old: {
+            nativeBuildInputs =
+              old.nativeBuildInputs
+              ++ final.resolveBuildSystem {
+                setuptools = [ ];
+              };
+          });
+        };
+
+        # Construct package set
+        pythonSet =
+          # Use base package set from pyproject.nix builders
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                overlay
+                pyprojectOverrides
+              ]
+            );
+
+          venv-pkg = (pythonSet.mkVirtualEnv "freetar-env" workspace.deps.default)
+            .overrideAttrs (old: {
+              meta = (old.meta or {}) // {
+                mainProgram = "freetar";
+              };
+            });
       in
       {
         packages = {
-          freetar = mkPoetryApplication {
-            projectDir = self;
-            overrides = defaultPoetryOverrides.extend
-              (self: super: {
-                lesscpy = super.lesscpy.overridePythonAttrs
-                  (old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
-                  });
-                flask-minify = super.flask-minify.overridePythonAttrs
-                  (old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
-                  });
-                itsdangerous = super.itsdangerous.overridePythonAttrs (
-                  old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ];
-                  }
-                );
-                jinja2 = super.jinja2.overridePythonAttrs (
-                  old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ];
-                  }
-                );
-              });
-          };
+          freetar = venv-pkg;
+          # freetar = pyproject-nix.build.util.mkApplication {
+          #   venv = venv-pkg;
+          #   package = pythonSet.freetar;
+          # };
           default = self.packages.${system}.freetar;
         };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.freetar ];
-          packages = [ pkgs.poetry ];
+          packages = [ venv-pkg pkgs.uv ];
+          env = {
+            UV_NO_SYNC = "1";
+            UV_PYTHON = "${venv-pkg}/bin/python";
+            UV_PYTHON_DOWNLOADS = "never";
+          };
+
+          shellHook = ''
+            # Undo dependency propagation by nixpkgs.
+            unset PYTHONPATH
+
+            # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+
+            echo 'use `uv run freetar` to start freetar'
+          '';
         };
       });
 }
